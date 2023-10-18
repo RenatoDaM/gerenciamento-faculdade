445b3f3caa9e27037dac36ec9bd6f36da304825b
Renato <renaato.d.m@gmail.com>
2023-10-18 17:20:47 -0300

    feat(execeptionhandler): criado handler de excessoes

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/exception/handler/ErrorResponse.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/exception/handler/ErrorResponse.java
@@ -1,2 +1,53 @@
-package com.gerenciamentofaculdade.gerenciamentofaculdade.exception.handler;public class ErrorResponse {
+package com.gerenciamentofaculdade.gerenciamentofaculdade.exception.handler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ErrorResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp;
+    int code;
+    String message;
+    List<String> detalhes;
+
+    public ErrorResponse(int code, String message, List<String> detalhes) {
+        this.code = code;
+        this.message = message;
+        this.detalhes = detalhes;
+        timestamp = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(List<String> detalhes) {
+        this.detalhes = detalhes;
+    }
 }
