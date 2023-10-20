cbc910ca3156fb0a8d31bbcc78e5c8007e6525b8
Renato <renaato.d.m@gmail.com>
2023-10-20 20:05:43 -0300

    feat(alunomapper): criado mapper entre alunomodel e alunodto

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/config/mapper/AlunoMapper.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/config/mapper/AlunoMapper.java
@@ -1,2 +1,14 @@
-package com.gerenciamentofaculdade.gerenciamentofaculdade.config.mapper;public class AlunoMapper {
+package com.gerenciamentofaculdade.gerenciamentofaculdade.config.mapper;
+
+import com.gerenciamentofaculdade.gerenciamentofaculdade.models.AlunoModel;
+import com.gerenciamentofaculdade.gerenciamentofaculdade.dto.AlunoDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AlunoMapper {
+    AlunoMapper INSTANCE = Mappers.getMapper( AlunoMapper.class );
+
+    AlunoModel dtoToModel(AlunoDTO alunoDto);
+    AlunoDTO modelToDTO(AlunoModel alunoModel);
 }
