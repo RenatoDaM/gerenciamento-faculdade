b660861adab4d658a0c83a869a37c705c89efa15
Renato <renaato.d.m@gmail.com>
2023-10-20 19:49:53 -0300

    feat(findbyra): criada nova assinatura para poder procurar aluno por ra, ajudando assim em tratamentos de erros e possiveis buscas

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/repository/AlunoRepository.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/repository/AlunoRepository.java
@@ -1,10 +1,10 @@
 package com.gerenciamentofaculdade.gerenciamentofaculdade.repository;
 
 import com.gerenciamentofaculdade.gerenciamentofaculdade.models.AlunoModel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository extends CrudRepository <AlunoModel, Long> {
-
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
+    AlunoModel findByRa(Long ra);
 }
