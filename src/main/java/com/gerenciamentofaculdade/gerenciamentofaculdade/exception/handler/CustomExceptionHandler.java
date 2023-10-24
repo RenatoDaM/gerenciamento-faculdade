afc5f820823dc7c063fc51d18573e842edb0231e
Renato <renaato.d.m@gmail.com>
2023-10-24 17:03:57 -0300

    feat(customexceptionhandler): criado exception handler com controller advice para dar mensagens customizadas referente a excecoes ao frontend

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/exception/handler/CustomExceptionHandler.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/exception/handler/CustomExceptionHandler.java
@@ -3,8 +3,8 @@ package com.gerenciamentofaculdade.gerenciamentofaculdade.exception.handler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.validation.ConstraintViolationException;
-import jdk.jfr.Event;
+import com.gerenciamentofaculdade.gerenciamentofaculdade.response.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
@@ -12,27 +12,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -67,10 +57,39 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    protected ResponseEntity<Object> argumentoInvalido(Exception ex) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse(400,"Bad Request", details);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add("Tipagem do path não compatível. Provavelmente você colocou valor string num campo que deveria ser numérico. Detalhe a seguir:");
+        details.add(ex.getLocalizedMessage());
+        if (details.get(1).equals("Failed to convert value of type 'java.lang.String' to required type 'java.lang.Long'; For input string: \"a\"")) {
+            details.set(1, "É necessário que a variável do path referente ao ID do aluno seja um valor numérico");
+        }
+        ErrorResponse error = new ErrorResponse(400,"Bad Request", details);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    protected ResponseEntity<Object> entidadeNaoEncontrada(Exception ex) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse(400,"Bad Request", details);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         List<String> details = new ArrayList<>();
-        details.add("Não foi possível ler a requisição. Verifique se o Body é válido e correspondente ao objeto do endpoint");
+        details.add("Não foi possível ler a requisição. Verifique se o Body é válido e correspondente ao objeto do endpoint, tanto o nome dos atributos quanto sua tipagem");
+        details.add("Erro: " + ex.getMessage());
         logger.error("Não foi possível ler uma requisição: " + ex.getLocalizedMessage());
         ErrorResponse error = new ErrorResponse(400,"Bad Request", details);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
