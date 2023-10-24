0f01cbcec3c5d41599527581844bc80b1921cfdf
Renato <renaato.d.m@gmail.com>
2023-10-24 17:01:28 -0300

    feat(dtos): criado dtos aluno e matricula para retornar apenas informacoes relevantes e nao sensiveis

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/dto/modeldto/AlunoDTO.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/dto/modeldto/AlunoDTO.java
@@ -1,25 +1,34 @@
-package com.gerenciamentofaculdade.gerenciamentofaculdade.dto;
+package com.gerenciamentofaculdade.gerenciamentofaculdade.dto.modeldto;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
 
 public class AlunoDTO {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
     @NotNull(message = "RA não pode estar em branco")
-    @Positive
-    private Long ra;
+    @Pattern(regexp = "^\\d{25}$", message = "RA deve ser um valor numérico contendo 25 dígitos")
+    private String ra;
 
     @NotBlank(message = "Nome não pode estar em branco")
     @NotNull(message = "Nome não pode ser nulo")
+    @NotEmpty(message = "Nome não pode estar vazio")
     private String nome;
+
+    @NotBlank(message = "E-mail não pode estar em branco")
+    @NotNull(message = "E-mail não pode ser nulo")
+    @Email(message = "Campo e-mail possui formato inválido")
+    String email;
 
     @NotBlank(message = "O primeiro telefone não pode estar em branco")
     @NotNull(message = "O primeiro telefone não pode ser nulo")
+    @NotEmpty(message = "O primeiro telefone não pode estar vazio")
+    @Pattern(regexp = "^\\d{11}$", message = "Telefone deve ser válido. Verifique se foi digitado um número a mais ou a menos")
     private String telefone1;
 
+    @Pattern(regexp = "^\\d{11}$", message = "Telefone deve ser válido")
     private String telefone2;
 
     public Long getId() {
@@ -30,11 +39,11 @@ public class AlunoDTO {
         this.id = id;
     }
 
-    public Long getRa() {
+    public String getRa() {
         return ra;
     }
 
-    public void setRa(Long ra) {
+    public void setRa(String ra) {
         this.ra = ra;
     }
 
@@ -44,6 +53,14 @@ public class AlunoDTO {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getTelefone1() {
--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/dto/modeldto/MatriculaDTO.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/dto/modeldto/MatriculaDTO.java
@@ -1,2 +1,42 @@
-package com.gerenciamentofaculdade.gerenciamentofaculdade.dto.modeldto;public class MatriculaDTO {
+package com.gerenciamentofaculdade.gerenciamentofaculdade.dto.modeldto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public class MatriculaDTO {
+    private Long id;
+
+    @NotNull(message = "Estado da matrícula não pode ser nulo")
+    @NotBlank(message = "Estado da matrícula não pode estar em branco")
+    @NotEmpty(message = "Estado da matrícula não pode ter espaços em branco")
+    private String estadoMatricula;
+
+    // semestre atual
+    @NotNull
+    private Integer ciclo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEstadoMatricula() {
+        return estadoMatricula;
+    }
+
+    public void setEstadoMatricula(String estadoMatricula) {
+        this.estadoMatricula = estadoMatricula;
+    }
+
+    public Integer getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(Integer ciclo) {
+        this.ciclo = ciclo;
+    }
 }
