364277c7ab505f5dde3d49f9d32ddc7f9efbf02f
Renato <renaato.d.m@gmail.com>
2023-10-24 17:05:15 -0300

    feat(matriculamodel): criada model referente a matricula

---
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/models/MatriculaModel.java
@@ -0,0 +1,81 @@
+package com.gerenciamentofaculdade.gerenciamentofaculdade.models;
+
+import com.gerenciamentofaculdade.gerenciamentofaculdade.enumeration.EstadoMatriculaEnum;
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity(name = "matricula")
+@Table(name = "matricula")
+public class MatriculaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EstadoMatriculaEnum estadoMatricula;
+
+    // semestre atual
+    @Column(columnDefinition = "integer", nullable = false)
+    private Integer ciclo;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false, columnDefinition = "date")
+    private Date dataMatricula;
+
+    @ManyToOne
+    @JoinColumn(name="aluno_id")
+    private AlunoModel alunoModel;
+
+    @ManyToOne
+    @JoinColumn(name="curso_id")
+    private CursoModel cursoModel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public EstadoMatriculaEnum getEstadoMatricula() {
+        return estadoMatricula;
+    }
+
+    public void setEstadoMatricula(String estadoMatricula) {
+        this.estadoMatricula = EstadoMatriculaEnum.valueOf(estadoMatricula);
+    }
+
+    public Integer getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(Integer ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public Date getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(Date dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public AlunoModel getAlunoModel() {
+        return alunoModel;
+    }
+
+    public void setAlunoModel(AlunoModel alunoModel) {
+        this.alunoModel = alunoModel;
+    }
+
+    public CursoModel getCursoModel() {
+        return cursoModel;
+    }
+
+    public void setCursoModel(CursoModel cursoModel) {
+        this.cursoModel = cursoModel;
+    }
+}
