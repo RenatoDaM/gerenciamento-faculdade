9772067f05dc5aa958470986c42052384599b39d
Renato <renaato.d.m@gmail.com>
2023-10-24 17:02:35 -0300

    feat(enumeration): criada classe enum para matricula para ter maior controle e seguranca ao definir o estado da matricula no banco de dados

--- src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/enumeration/EstadoMatriculaEnum.java
+++ src/main/java/com/gerenciamentofaculdade/gerenciamentofaculdade/enumeration/EstadoMatriculaEnum.java
@@ -1,2 +1,15 @@
-package com.gerenciamentofaculdade.gerenciamentofaculdade.enumeration;public enum EstadoMatriculaEnum {
+package com.gerenciamentofaculdade.gerenciamentofaculdade.enumeration;
+
+public enum EstadoMatriculaEnum {
+    ATIVA("ATIVA"), TRANCADA("TRANCADA"), CONCLUIDA("CONCLUIDA");
+
+    private String value;
+
+    EstadoMatriculaEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
